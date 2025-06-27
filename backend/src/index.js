655ad77e5979ec754ef94fad9d1464667622e61e
Sabari <sabarisanthosh85@gmail.com>
2025-06-27 14:12:19 +0530

    Mongo Db connection Updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,8 +46,7 @@ app.use('/api/notifications', require('./routes/notification'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
